caaee13a9e908b658b115e0fca18af59e780279d
migue2509 <miguel.ospina.dev@gmail.com>
2026-09-13 16:33:09 -0500

    feat(about): incorporar tecnologías agrupadas en la sección sobre mí

---
+++ Portfolio/src/data/technologies.ts
@@ -0,0 +1,28 @@
+import htmlIcon from "../assets/technologies/html.png";
+import cssIcon from "../assets/technologies/css.png";
+import javascriptIcon from "../assets/technologies/javascript.png";
+import javaIcon from "../assets/technologies/java.png";
+import springBootIcon from "../assets/technologies/spring-boot.png";
+import pythonIcon from "../assets/technologies/python.png";
+import postgresqlIcon from "../assets/technologies/postgresql.png";
+import gitIcon from "../assets/technologies/git.png";
+import vscodeIcon from "../assets/technologies/vscode.png";
+import intellijIcon from "../assets/technologies/intellij.png";
+import figmaIcon from "../assets/technologies/figma.png";
+
+export const technologies = {
+  html: { name: "HTML", icon: htmlIcon },
+  css: { name: "CSS", icon: cssIcon },
+  javascript: { name: "JavaScript", icon: javascriptIcon },
+  java: { name: "Java", icon: javaIcon },
+  springBoot: { name: "Spring Boot", icon: springBootIcon },
+  python: { name: "Python", icon: pythonIcon },
+  postgresql: { name: "PostgreSQL", icon: postgresqlIcon },
+  git: { name: "Git", icon: gitIcon },
+  vscode: { name: "VS Code", icon: vscodeIcon },
+  intellij: { name: "IntelliJ IDEA", icon: intellijIcon },
+  figma: { name: "Figma", icon: figmaIcon },
+  postman: { name: "Postman", icon: null },
+} as const;
+
+export type TechnologyId = keyof typeof technologies;
